0f93ec051bfcf45ad5d8fa7875edb598dbd7db2a
Aleksandar Vasilev <av@mega.co.nz>
2014-07-31 15:51:38 +0300

    Whitespace cleanup

--- src/strophe.jingle.session.h
+++ src/strophe.jingle.session.h
@@ -11,52 +11,52 @@
 namespace karere {
 namespace rtcModule {
 
-	class Jingle;
-	class JingleEventHandler; //can't nest it into Jingle
+    class Jingle;
+    class JingleEventHandler; //can't nest it into Jingle
 
 class JingleSession: public webrtc::PeerConnectionObserver
 {
 public:
-	enum State {
-		SESSTATE_NULL = 0,
-		SESSTATE_ENDED = 1,
-		SESSTATE_ACTIVE = 2,
-		SESSTATE_ERROR = 3,
-		SESSTATE_PENDING = 4
-	};
-	struct AvFlags
-	{
-		bool audio = false;
-		bool video = false;
-	};
-	struct MediaConstraints
-	{
-		bool audio = false;
-		bool video = false;
-		int width = 0;
-		int height = 0;
-	};
+    enum State {
+        SESSTATE_NULL = 0,
+        SESSTATE_ENDED = 1,
+        SESSTATE_ACTIVE = 2,
+        SESSTATE_ERROR = 3,
+        SESSTATE_PENDING = 4
+    };
+    struct AvFlags
+    {
+        bool audio = false;
+        bool video = false;
+    };
+    struct MediaConstraints
+    {
+        bool audio = false;
+        bool video = false;
+        int width = 0;
+        int height = 0;
+    };
 
 protected:
     rtc::myPeerConnection<JingleSession> mPeerConn;
-	std::string mMyJid;
-	std::string mPeerJid;
-	std::string mSid;
+    std::string mMyJid;
+    std::string mPeerJid;
+    std::string mSid;
     strophe::Connection& mConnection;
-	Jingle& mJingle;
-	std::string mInitiator;
-	std::string mResponder;
-	bool mIsInitiator;
-	State mState = SESSTATE_NULL;
+    Jingle& mJingle;
+    std::string mInitiator;
+    std::string mResponder;
+    bool mIsInitiator;
+    State mState = SESSTATE_NULL;
 
-	AvFlags mLocalMutedState;
-	AvFlags mRemoteMutedState;
-	rtc::tspMediaStream mLocalStream;
-	rtc::tspMediaStream mRemoteStream;
+    AvFlags mLocalMutedState;
+    AvFlags mRemoteMutedState;
+    rtc::tspMediaStream mLocalStream;
+    rtc::tspMediaStream mRemoteStream;
     sdpUtil::ParsedSdp mLocalSdp;
     sdpUtil::ParsedSdp mRemoteSdp;
-	int mStartTime = 0;
-	int mEndTime = 0;
+    int mStartTime = 0;
+    int mEndTime = 0;
     webrtc::FakeConstraints mMediaConstraints;
     std::string mPeerNonce;
     bool mLastIceCandidate = false;
@@ -64,51 +64,51 @@ protected:
     void addFingerprintHmac(strophe::Stanza jingle);
 //PeerConnection callback interface
 public:
-	void onError() {KR_LOG_ERROR("session %s: peerconnection called onError()", mSid.c_str());}
-	void onAddStream(rtc::tspMediaStream stream);
-	void onRemoveStream(rtc::tspMediaStream stream);
-	void onIceCandidate(std::shared_ptr<rtc::IceCandText> candidate);
-	void onIceComplete();
-	void onSignalingChange(webrtc::PeerConnectionInterface::SignalingState newState){}
-	void onIceConnectionChange(webrtc::PeerConnectionInterface::IceConnectionState newState);
-	void onRenegotiationNeeded() {}
+    void onError() {KR_LOG_ERROR("session %s: peerconnection called onError()", mSid.c_str());}
+    void onAddStream(rtc::tspMediaStream stream);
+    void onRemoveStream(rtc::tspMediaStream stream);
+    void onIceCandidate(std::shared_ptr<rtc::IceCandText> candidate);
+    void onIceComplete();
+    void onSignalingChange(webrtc::PeerConnectionInterface::SignalingState newState){}
+    void onIceConnectionChange(webrtc::PeerConnectionInterface::IceConnectionState newState);
+    void onRenegotiationNeeded() {}
     strophe::Stanza createJingleIq(const std::string& to, const char* action);
     int getMlineIndex(const sdpUtil::ParsedSdp& sdp, std::string& name);
 //==
 
-	JingleSession(Jingle& jingle, const	std::string& myJid,
-		const std::string& peerJid,	const std::string& sid,
+    JingleSession(Jingle& jingle, const	std::string& myJid,
+        const std::string& peerJid,	const std::string& sid,
         strophe::Connection& connection, rtc::tspMediaStream sessLocalStream,
         const AvFlags& mutedState);
-	void initiate(bool isInitiator);
+    void initiate(bool isInitiator);
     promise::Promise<strophe::Stanza> accept();
     promise::Promise<strophe::Stanza> sendOffer();
     void terminate(const std::string& reason); //TODO: maybe not needed
-	inline bool isActive()
-	{
+    inline bool isActive()
+    {
          return ((mPeerConn.get() != NULL)
          &&	(mState != SESSTATE_ENDED)
-		 && (mState != SESSTATE_ERROR)
+         && (mState != SESSTATE_ERROR)
          &&	(mPeerConn.get()->signaling_state() !=
-			 webrtc::PeerConnectionInterface::kClosed));
-	}
-	inline void checkActive(const char* opname)
-	{
-		if (!isActive())
+             webrtc::PeerConnectionInterface::kClosed));
+    }
+    inline void checkActive(const char* opname)
+    {
+        if (!isActive())
             throw std::runtime_error((opname?opname:"")+std::string(": Session '")+mSid+ "' to '"+mPeerJid+
-			"' is not active anymore or peerconnection has been closed");
-	}
-	inline const char* jCreator() const
-	{
+            "' is not active anymore or peerconnection has been closed");
+    }
+    inline const char* jCreator() const
+    {
         return (mIsInitiator?"initiator":"responder");
-	}
+    }
     promise::Promise<strophe::Stanza> sendIceCandidate(std::shared_ptr<rtc::IceCandText> candidate);
     promise::Promise<int> setRemoteDescription(strophe::Stanza stanza, const std::string& desctype);
     void addIceCandidate(strophe::Stanza stanza);
     void addIceCandidates(strophe::Stanza transportInfo);
-	promise::Promise<int> sendAnswer();
+    promise::Promise<int> sendAnswer();
     promise::Promise<strophe::Stanza> sendTerminate(const std::string& reason, const std::string& text);
-	promise::Promise<int> sendIq(const karere::StringMap& attrs, const std::string& desc);
+    promise::Promise<int> sendIq(const karere::StringMap& attrs, const std::string& desc);
     promise::Promise<strophe::Stanza> sendMute(bool muted, const std::string& what);
     void syncMutedState();
     promise::Promise<int> sendMutedState();
