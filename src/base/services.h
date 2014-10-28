513755e20c200764b97f7280002deec16f5b5638
Alexander Vasilev <av@mega.co.nz>
2014-10-28 19:32:27 +0200

    Added base/services.h

---
+++ src/base/services.h
@@ -0,0 +1,29 @@
+#ifndef _MEGA_BASE_SERVICES_INCLUDED
+#define _MEGA_BASE_SERVICES_INCLUDED
+
+namespace mega
+{
+
+template <class CB>
+void* setTimeout(CB callback, int time)
+{
+    //TODO: Implement
+}
+void cancelTimeout(void* handle)
+{
+    //TODO: implement
+}
+
+template <class CB>
+void* setInterval(CB callback, int time)
+{
+    //TODO: Implement
+}
+void cancelInterval(void* handle)
+{
+    //TODO: Implement
+}
+
+
+}
+#endif
