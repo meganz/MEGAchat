0a1447ee2558bd609ca647ec3a09600d69c9be83
Alexander Vasilev <av@mega.co.nz>
2014-11-22 20:47:13 +0200

    Refactored the gui call marshaller to use plain C interface, and implemented the C++11 features in services.h

--- src/base/guiCallMarshaller.h
+++ src/base/guiCallMarshaller.h
@@ -1,127 +1,92 @@
 #ifndef GUICALLMARSHALLER_H
 #define GUICALLMARSHALLER_H
-#include <stdexcept>
-#include <utility>
-#include <functional>
-#include <memory>
 
-namespace mega
-{
+/* This is a plain C header */
 
-/** The common base message type that gets passed through the app's native GUI
- * message loop. Any memory associated with the message object can be managed only
- * by the shared object that created it, because each shared object is likely to
- * have its own memory management. This includes both directly allocating/freeing
- * members of the message, or calling any methods of members that may try to
- * do memory management on memory associated with the message. For example -
- * method calls on STL container members of the message.
- * Further - if the shared object that created the message and the one that accesses
- * it can be built with different compilers, the C++ ABIs must be considered incompatible.
- * Therefore, only the header part of the message can be accessed, up till a C++ member
- * appears.
-*/
-struct Message
-{
-    enum _magic: unsigned long {kMegaMsgMagic = 0x3e9a3591};
-
-/** Constants that denote the type of the message so that the processMessages()
- *  dispatcher can cast and handle them accordingly
- */
-    enum {kMsgFuncCall = 1 << 0, kMsgWithHandler = 1 << 1};
-/** The message type field */
-    const unsigned char type;
-    unsigned long magic;
-    Message(unsigned char aType)
-        :type(aType), magic(kMegaMsgMagic){}
-	inline void verify()
-	{
-#ifndef NDEBUG
-        if (magic != kMegaMsgMagic)
-			throw std::runtime_error("Message does not have the correct magic value");
+#ifdef _WIN32
+#define MEGA_GCM_EXPORT __declspec(dllexport)
+#else
+#define MEGA_GCM_EXPORT __attribute__ ((visibility ("default")))
 #endif
-    }
-    virtual ~Message()
-    {
-#ifndef NDEBUG
-        magic = 0;
+
+struct megaMessage;
+typedef void(*megaMessageFunc)(struct megaMessage*);
+
+#ifdef __cplusplus
+extern "C"
+{
 #endif
-    }
-};
 
-/** This types of messages contain the message handler function pointer within themselves
- * Their type has bit 30 set, so that the handler can recognized and process them properly
- */
-struct MessageWithHandler: public Message
-{
-    /** Handler C function that the message will be sent to, when received by the GUI thread
-     * Dispatching messages by directly specifying the handler function allows
-     * to dispatch the same kind of message to the correct module(shared object/main app)
-     * so that access/memory management on the message is always performed by the module
-     * that created the message.
-     */
-    typedef void(*HandlerFunc)(Message*);
-    HandlerFunc handler;
-    MessageWithHandler(HandlerFunc aHandler)
-            :Message(Message::kMsgWithHandler), handler(aHandler){}
-};
-
-struct AbstractFuncCallMessage: public Message
-{
-    AbstractFuncCallMessage(): Message(Message::kMsgFuncCall){}
-    virtual void doCall() = 0;
-    virtual ~AbstractFuncCallMessage() {}
-};
-
-/**
-* Message that posts a function to be called by the GUI thread.
-* The function can be a c-style function, a lambda or a std::function object
+/** The mechanism supports marshalling of a plain C function with signature void(void*)
+ * The Message structure carries the function pointer and the data is a pointer to
+ * the Message struct itself. The actual object passed is derived from \c Message and can
+ * contain arbitrary data, so \c struct \c Message acts as the header of the actual structure
+ * passed. As the handler knows the actual type of the structure, it casts it to the proper
+ * derived type and accesses the additional data after the \c Message header. In other words,
+ * this is plain C polymorphism, and this API is plain C to make possible interoperation
+ * between modules built with different compilers, even different languages.
+ *
+ * All memory associated with the message object is managed on the same side of the
+ * shared-object boundary (the one that posts the message),
+ * because only that side knows the exact type of the message
+ * object, and also because each shared object is likely to
+ * have its own memory management and even be built with a different compiler (C++ ABIs
+ * are incompatible).
 */
-template <class F>
-struct FuncCallMessage: public AbstractFuncCallMessage
+
+struct megaMessage
 {
-    F mFunc;
-    FuncCallMessage(F&& aFunc)
-        :AbstractFuncCallMessage(), mFunc(std::forward<F>(aFunc))
-	{}
-    virtual void doCall()	{ mFunc(); }
+    megaMessageFunc func;
+#ifdef __cplusplus
+//if we don't provice an initializing constructor, operator new() will initialize func
+//to NULL, and then we will overwrite it, which is inefficient.
+    megaMessage(megaMessageFunc aFunc): func(aFunc){}
+#endif
 };
+//enum {kMegaMsgMagic = 0x3e9a3591};
 
-/** Must be provided by the user.
-* Wraps an opaque void*, poiting to a mega message, into a
-* platform inter-thread message/signal and posts it to the GUI
-* message loop. This function is called by various threads
+/** This function posts an opaque \c void* to the application's (GUI) message loop
+* and that message is then received by the application's main (GUI) thread and sent to
+* the \c megaProcessMessage(void*) to be handled by the receiving code.
+* \warning This function must be provided by the user and is specific to the GUI framework
+* used in the app. It is called by various threads when they need to execute a function
+* call by the main (GUI) thread.
 */
-void postMessageToGui(void* msg);
 
-/** Utility end-user function that uses the messaging infrastructure to marshal a function call
- * to the GUI thread. Can be used also to call a function asynchronously, similar to
- * setTimeout(..., 0) in javascript
+#ifndef MEGA_GCM_CLIENT_INIT_API
+    void megaPostMessageToGui(void* msg);
+#else
+    typedef void(*megaGcmPostFunc)(void*);
+    extern megaGcmPostFunc megaPostMessageToGui;
+    #define _MEGA_GCM_MAKESYM(prefix, suffix) prefix##suffix
+    #define MEGA_GCM_MAKESYM(prefix, suffix) _MEGA_GCM_MAKESYM(prefix, suffix)
+/** This function is defined in all dynamic link libraries that want to use the gui
+ * call marshaller \c megaPostMessageToGui() function via a pointer passed to an init
+ * function, rather than dynamically linking to the function in the main app executable.
+ * This technique can be used if dynamic linking does not work or is problematic.
+ * @returns On success returns 0, otherwise an error code.
 */
-template <class F>
-static inline void marshalCall(F&& func)
-{
-    FuncCallMessage<F>* msg = new FuncCallMessage<F>(std::forward<F>(func));
-	postMessageToGui((void*)msg); //platform-specific, user-defined
-}
+    int MEGA_GCM_EXPORT MEGA_GCM_MAKESYM(megaGcmInit_, MEGA_GCM_CLIENT_INIT_API)(megaGcmPostFunc postFunc);
+#endif
 
-/** This function must be called by the user when a Mega Message has been received
-* by the GUI thread
-* It passes a received by the GUI thread to the MegaSDK for processing.
+/** When the application's main (GUI) thread receives a message posted by
+ * megaPostMessageToGui(), it must forward the \c void* pointer
+ * from that message to this function for further processing. This function is
+ * called by a handler in the app's (GUI) event/message loop (or equivalent, such as
+ * a QT slot called on the GUI thread) and normally it should be called only
+ * from one place in the code that serves as a bridge between the native message
+ * loop and the mega messaging system.
 * \warning Must be called only from the GUI thread
 */
-static inline void processMessage(void* voidPtr)
+
+static inline void megaProcessMessage(void* vptr)
 {
-    std::unique_ptr<Message> msg(static_cast<Message*>(voidPtr));
-	msg->verify();
-    const unsigned char type = msg->type;
-    if (type == Message::kMsgFuncCall)
-        (static_cast<AbstractFuncCallMessage*>(msg.get()))->doCall();
-    else if (type == Message::kMsgWithHandler)
-        (static_cast<MessageWithHandler*>(msg.get()))->handler(msg.get());
-    else
-        fprintf(stderr, "Unknown message type: %X", type);
+    struct megaMessage* msg = (struct megaMessage*)vptr;
+    msg->func(msg);
 }
 
-}
+#ifdef __cplusplus
+} //end extern "C"
+#endif
 
 #endif // GUICALLMARSHALLER_H
--- src/base/services.h
+++ src/base/services.h
@@ -1,8 +1,32 @@
 #ifndef _MEGA_BASE_SERVICES_INCLUDED
 #define _MEGA_BASE_SERVICES_INCLUDED
 
+#include "guiCallMarshaller.h"
+#include <memory>
+#include <assert.h>
+
 namespace mega
 {
+template <class F>
+static inline void marshallCall(F&& func)
+{
+    struct Msg: public megaMessage
+    {
+        F mFunc;
+        Msg(F&& aFunc, megaMessageFunc cHandler): megaMessage(cHandler), mFunc(aFunc){}
+#ifndef NDEBUG
+        unsigned magic = 0x3e9a3591;
+#endif
+    };
+    Msg* msg = new Msg(std::forward<F>(func),
+    [](megaMessage* ptr)
+    {
+        std::unique_ptr<Msg> pMsg(static_cast<Msg*>(ptr));
+        assert(pMsg->magic == 0x3e9a3591);
+        pMsg->mFunc();
+    });
+    megaPostMessageToGui(static_cast<void*>(msg));
+}
 
 template <class CB>
 static inline void* setTimeout(CB callback, int time)
