55b8ca13567c1095097bbf220b328e0f882bc120
Alexander Vasilev <av@mega.co.nz>
2015-07-23 22:28:11 +0300

    Some more promise unit tests

--- src/base/promise-test.cpp
+++ src/base/promise-test.cpp
@@ -8,7 +8,7 @@ using namespace promise;
 int main()
 {
 
-TestGroup("general")
+TestGroup("General")
 {
   asyncTest("Promise<int>: resolve->then(ret error)->fail(recover)->then",
   {{"fail", "order", 2}, {"then1", "order", 1}, {"then2", "order", 3}})
@@ -69,7 +69,7 @@ TestGroup("general")
       loop.schedCall([pms]() mutable
           {  pms.resolve();  }, 100);
   });
-  asyncTest("rejected should skip all then()-s", {"fail1"})
+  asyncTest("Rejected should skip all then()-s", {"fail1"})
   {
       Promise<void> pms;
       pms.then([&]()
@@ -87,7 +87,7 @@ TestGroup("general")
       pms.reject("test message");
   });
 
-  asyncTest("change type", {"then1", "then2", "then-final"})
+  asyncTest("Change type", {"then1", "then2", "then-final"})
   {
       Promise<int> pms;
       pms.then([&](int a)
@@ -105,7 +105,7 @@ TestGroup("general")
       });
       pms.resolve(1212);
   });
-  asyncTest("should default-return resolved Promise<void>", {"then1", "then-final"})
+  asyncTest("Should default-return resolved Promise<void>", {"then1", "then-final"})
   {
       Promise<int> pms;
       pms.then([&](int a) mutable
@@ -118,7 +118,7 @@ TestGroup("general")
       });
       pms.resolve(1234);
   });
-  asyncTest("should propagete failure through void callback (r180f82)", {"then"})
+  asyncTest("Should propagete failure through void callback (r180f82)", {"then"})
   {
       Promise<int> pms;
       pms.then([&](int a)
@@ -177,7 +177,7 @@ TestGroup("general")
       pms.resolve(1);
   });
 
-  asyncTest("should propagate resolution from nested scope",
+  asyncTest("Should propagate resolution from nested scope",
   {{"inner", "order", 1}, {"then-final", "order", 2}})
   {
       Promise<int> pms;
@@ -223,6 +223,146 @@ TestGroup("general")
       pms.resolve(1);
   });
 });
+
+TestGroup("Exception tests")
+{
+    asyncTest("Should fail if std::exception in then() block")
+    {
+        Promise<int> pms;
+        pms.then([&](int a)
+        {
+            check(a == 1);
+            throw std::runtime_error("test exception");
+        })
+        .then([&]()
+        {
+            test.error("should not execute then() after a then() with exception");
+            return;
+        })
+        .fail([&](const Error& err)
+        {
+            doneOrError(err.msg() == "test exception", );
+        });
+        pms.resolve(1);
+    });
+    asyncTest("Should fail with message if char* exception in then() block", {"fail"})
+    {
+        Promise<int> pms;
+        pms.then([&](int a)
+        {
+            check(a == 1);
+            throw "test char* exception";
+        })
+        .then([&]()
+        {
+            test.error("should not execute then() after a then() with exception");
+            return;
+        })
+        .fail([&](const Error& err)
+        {
+            doneOrError(err.msg() == "test char* exception", "fail");
+        });
+        pms.resolve(1);
+    });
+    asyncTest("Should fail if non-std exception in then() block", {"fail"})
+    {
+        Promise<int> pms;
+        pms.then([&](int a)
+        {
+            check(a == 1);
+            throw 10;
+        })
+        .then([&]()
+        {
+            test.error("should not execute then() after a then() with exception");
+            return;
+        })
+        .fail([&](const Error& err)
+        {
+            test.done("fail");
+        });
+        pms.resolve(1);
+    });
+    asyncTest("Should fail if exception in fail() block", {"fail", "fail excep"})
+    {
+        Promise<int> pms;
+        pms.then([&](int)
+        {
+            test.error("Should not execute then() on failed promise");
+        })
+        .fail([&](const Error& err)
+        {
+            doneOrError(err.msg() == "test message", "fail");
+            throw std::runtime_error("Test exception");
+        })
+        .fail([&](const Error& err)
+        {
+            doneOrError(err.msg() == "Test exception", "fail excep");
+        });
+        pms.reject("test message");
+    });
+});
+TestGroup("Double resolve tests")
+{
+    asyncTest("Should throw if trying to resolve resolved promise", {"excep"})
+    {
+        Promise<int> pms;
+        pms.resolve(1);
+        check(pms.done() == Promise<int>::PROMISE_RESOLV_SUCCESS);
+        try
+        {
+            pms.resolve(2);
+        }
+        catch(...)
+        {
+            test.done("excep");
+        }
+    });
+    asyncTest("Should throw if trying to resolve failed promise", {"excep"})
+    {
+        Promise<int> pms;
+        pms.reject("test error");
+        check(pms.done() == Promise<int>::PROMISE_RESOLV_FAIL);
+        try
+        {
+            pms.resolve(2);
+        }
+        catch(...)
+        {
+            test.done("excep");
+        }
+    });
+    asyncTest("Should throw if trying to reject resolved promise", {"excep"})
+    {
+        Promise<int> pms;
+        pms.resolve(1);
+        check(pms.done() == Promise<int>::PROMISE_RESOLV_SUCCESS);
+        try
+        {
+            pms.reject("test");
+        }
+        catch(...)
+        {
+            test.done("excep");
+        }
+    });
+    asyncTest("Should throw if trying to reject a failed promise", {"excep"})
+    {
+        Promise<int> pms;
+        pms.reject("test1");
+        check(pms.done() == Promise<int>::PROMISE_RESOLV_FAIL);
+        try
+        {
+            pms.reject("test2");
+        }
+        catch(...)
+        {
+            test.done("excep");
+        }
+    });
+
+});
+
 TestGroup("when() tests")
 {
     asyncTest("when() with 2 already resolved promises")
@@ -249,7 +389,7 @@ TestGroup("when() tests")
         });
         loop.schedCall([pms2]() mutable { pms2.resolve(); }, 100);
     });
-    asyncTest("when() with 2 async-resolved promise")
+    asyncTest("when() with 2 async-resolved promises")
     {
         Promise<int> pms1;
         Promise<void> pms2;
@@ -293,6 +433,64 @@ TestGroup("when() tests")
         });
         loop.schedCall([pms1, &test]() mutable { test.done("first"); pms1.resolve(); }, -100);
         loop.schedCall([pms2, &test]() mutable { test.done("second"); pms2.reject("Test message");}, -100);
+    });
+    asyncTest("when() with two already failed promises")
+    {
+        Promise<int> pms1(Error("first rejected"));
+        Promise<void> pms2(Error("second rejected"));
+        when(pms1, pms2)
+        .then([&]()
+        {
+            test.error("Output should not succeed");
+        })
+        .fail([&](const Error& err)
+        {
+            doneOrError(err.msg() == "first rejected",);
+        });
+    });
+    asyncTest("when() with four async-resolved promises",{
+     {"first", "order", 1}, {"second", "order", 2}, {"third", "order", 3},
+     {"fourth", "order", 4}, {"output", "order", 5}})
+    {
+        Promise<void> pms1;
+        Promise<int> pms2;
+        Promise<const char*> pms3;
+        Promise<std::string> pms4;
+        when(pms1, pms2, pms3, pms4)
+        .then([&]()
+        {
+            test.done("output");
+        })
+        .fail([&](const Error& err)
+        {
+            test.error("Output promise should not fail()");
+        });
+        loop.schedCall([pms1, &test]() mutable { test.done("first"); pms1.resolve(); }, -100);
+        loop.schedCall([pms2, &test]() mutable { test.done("second"); pms2.resolve(1);}, -100);
+        loop.schedCall([pms3, &test]() mutable { test.done("third"); pms3.resolve("test"); }, -100);
+        loop.schedCall([pms4, &test]() mutable { test.done("fourth"); pms4.resolve("test fourth"); }, -100);
+    });
+    asyncTest("when() with 3 async-resolved promises, and one async-failed",{
+     {"first", "order", 1}, {"second", "order", 2}, {"third", "order", 3},
+     {"fourth", "order", 4}, {"output", "order", 5}})
+    {
+        Promise<void> pms1;
+        Promise<int> pms2;
+        Promise<const char*> pms3;
+        Promise<std::string> pms4;
+        when(pms1, pms2, pms3, pms4)
+        .then([&]()
+        {
+            test.error("Output promise should fail, not call then()");
+        })
+        .fail([&](const Error& err)
+        {
+            doneOrError(err.msg() == "test fourth fail", "output");
+        });
+        loop.schedCall([pms1, &test]() mutable { test.done("first"); pms1.resolve(); }, -100);
+        loop.schedCall([pms2, &test]() mutable { test.done("second"); pms2.resolve(1);}, -100);
+        loop.schedCall([pms3, &test]() mutable { test.done("third"); pms3.resolve("test"); }, -100);
+        loop.schedCall([pms4, &test]() mutable { test.done("fourth"); pms4.reject("test fourth fail"); }, -100);
     });
 
 });
