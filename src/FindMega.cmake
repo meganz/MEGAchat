311e7e8b3334d4ee30f092ff764749ff5efa01a6
Alexander Vassilev <av@mega.co.nz>
2016-04-28 14:43:38 +0300

    Updated FindMEga.cmake

--- src/FindMega.cmake
+++ src/FindMega.cmake
@@ -10,20 +10,43 @@ find_path(LIBMEGA_PUBLIC_INCLUDE_DIR megaapi.h
 find_library(_LIBMEGA_LIBRARIES mega
     HINTS ${PC_LIBMEGA_LIBDIR} ${PC_LIBMEGA_LIBRARY_DIRS}
 )
+
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(LibMega DEFAULT_MSG
     _LIBMEGA_LIBRARIES LIBMEGA_PUBLIC_INCLUDE_DIR
 )
 
-if (NOT WIN32)
-    pkg_check_modules(PC_CARES QUIET libcares)
-    find_library(CARES_LIB cares HINTS ${PC_CARES_LIBDIR} ${PC_CARES_LIBRARY_DIRS})
-    if (NOT CARES_LIB)
-        message(FATAL_ERROR "Could not find c-ares library, needed by Mega SDK")
+# For the libs that don't have FindXXX cmake modules
+macro(findlib varprefix name header libnames)
+    pkg_check_modules("${varprefix}" QUIET "${name}")
+    if (NOT ${name}_FOUND)
+        #look in the standard paths, can be set on the commandline via -DCMAKE_PREFIX_PATH=... or -DCMAKE_FIND_ROOT=...
+        set(flhdr "${varprefix}_INCLUDE_DIRS")
+        find_path("${flhdr}" "${header}")
+        if ("${${flhdr}}" STREQUAL "${flhdr}-NOTFOUND")
+            message(FATAL_ERROR "ERROR: library ${name} not found")
+        else()
+            # lib header found in standard paths, possibly in CMAKE_PREFIX_PATH,
+            # so it should work out of the box as long as we add CMAKE_PREFIX_PATH
+            # to the include and library dirs that are passed to the compiler.
+            # No need to set ${varprefix}_INCLUDE_DIRS
+            # quite possibly, lib files will also be in standard paths,
+            # so we just put their names without paths
+            set(${varprefix}_LIBRARIES ${libnames})
+            message(STATUS "Found ${name}")
+        endif()
     endif()
-    #c-ares is the only megasdk dependency that we don't already include,
-    #so add it to the megasdk libs
-    list(APPEND _LIBMEGA_LIBRARIES ${CARES_LIB})
+endmacro()
+
+findlib(SQLITE sqlite3 sqlite3.h sqlite3)
+findlib(CRYPTOPP libcrypto++ "cryptopp/cryptlib.h" cryptopp)
+findlib(CARES libcares ares.h cares)
+find_package(CURL REQUIRED)
+
+list(APPEND _LIBMEGA_LIBRARIES ${CURL_LIBRARIES} ${CARES_LIBRARIES} ${CRYPTOPP_LIBRARIES}
+    ${SQLITE_LIBRARIES})
+
+if (NOT WIN32)
     set(platdir posix)
 else()
     if (WINPHONE)
@@ -44,6 +67,7 @@ endif()
 set(LIBMEGA_INCLUDE_DIRS 
     "${LIBMEGA_PUBLIC_INCLUDE_DIR}"
     "${LIBMEGA_PUBLIC_INCLUDE_DIR}/mega/${platdir}"
+    "${CURL_INCLUDE_DIRS}" "${CARES_INCLUDE_DIRS}" "${CRYPTOPP_INCLUDE_DIRS}" "${SQLITE_INCLUDE_DIRS}"
     CACHE STRING "" FORCE
 )
 
