a9fdd67ad318f4f2410dbcdb228a0d2ce3e3d779
Alexander Vassilev <av@mega.co.nz>
2016-07-12 14:49:23 +0300

    Updated sqlite download link in setup-deps.sh

--- platforms/setup-deps.sh
+++ platforms/setup-deps.sh
@@ -442,7 +442,7 @@ if [[ "$platform" != linux ]]; then
     fetchInstall cryptopp  "https://www.cryptopp.com/cryptopp563.zip" "" "" "cryptopp563"
     fetchInstall libsodium "https://download.libsodium.org/libsodium/releases/libsodium-1.0.10.tar.gz"
     fetchInstall expat     "http://downloads.sourceforge.net/project/expat/expat/2.1.1/expat-2.1.1.tar.bz2?r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fexpat%2F&ts=1458829388&use_mirror=heanet" "" expat-2.1.1.tar.bz2
-    fetchInstall sqlite    "https://www.sqlite.org/2016/sqlite-autoconf-3130000.tar.gz"
+    fetchInstall sqlite    "https://www.sqlite.org/2016/sqlite-autoconf-3130000.tar.gz" "" "" "sqlite"
     # android NDK ships with zlib
     if [[ "$platform" != "android" ]]; then
         fetchInstall zlib      "http://zlib.net/zlib-1.2.8.tar.gz"
