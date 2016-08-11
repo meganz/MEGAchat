a5c03f1081e5493a642489979041b4084735fc92
Alexander Vassilev <av@mega.co.nz>
2016-08-11 17:13:21 +0300

    code cleanup

--- src/rtcModule/IRtcModule.h
+++ src/rtcModule/IRtcModule.h
@@ -20,6 +20,7 @@
  * You should have received a copy of the license along with this
  * program.
  */
+
 #include "mstrophepp.h" //only needed for IPlugin
 #include "karereCommon.h" //for AvFlags
 #ifdef _WIN32
