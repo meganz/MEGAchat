4db0f5d4c905285c8c7faf4efff400f29e5f6226
Sergio Hernández <shs@mega.co.nz>
2016-12-28 18:04:48 +0100

    Fix validity checkup for myEmail

--- src/chatClient.cpp
+++ src/chatClient.cpp
@@ -531,7 +531,7 @@ std::string Client::getMyEmailFromDb()
 
     std::string email = stmt.stringCol(0);
 
-    if (email.empty() || email.length() == 1)
+    if (email.length() < 5)
         throw std::runtime_error("loadOwnEmailFromDb: Own email in db is invalid");
     return email;
 }
