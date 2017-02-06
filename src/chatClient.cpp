cf88ec9c9e9eb39e506dea403fe6d41d2480b2e8
Alexander Vassilev <av@mega.co.nz>
2017-02-06 18:25:40 +0200

    Fix gcc comile issue

--- src/chatClient.cpp
+++ src/chatClient.cpp
@@ -577,7 +577,7 @@ promise::Promise<void> Client::connect(Presence pres)
     promise::Promise<void> connectPromise;
 
     mCanConnectPromise
-    .then([this, pres, connectPromise]()
+    .then([this, pres, connectPromise]() mutable
     {
         doConnect(pres)
         .then([connectPromise]() mutable
