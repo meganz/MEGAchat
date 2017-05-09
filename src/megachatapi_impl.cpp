75d4332d5531221dab111d908c07b09e6bbefd5f
Sergio Hernández <shs@mega.nz>
2017-05-09 18:53:55 +0200

    Check if a message can be deleted or not

--- src/megachatapi_impl.cpp
+++ src/megachatapi_impl.cpp
@@ -4105,6 +4105,12 @@ bool MegaChatMessagePrivate::isEditable() const
     return (type == TYPE_NORMAL && !isDeleted() && ((time(NULL) - ts) < CHATD_MAX_EDIT_AGE));
 }
 
+bool MegaChatMessagePrivate::isDeletable() const
+{
+    return ((type == TYPE_NORMAL || type == TYPE_CONTACT_ATTACHMENT)
+            && !isDeleted() && ((time(NULL) - ts) < CHATD_MAX_EDIT_AGE));
+}
+
 bool MegaChatMessagePrivate::isManagementMessage() const
 {
     return (type == TYPE_ALTER_PARTICIPANTS ||
