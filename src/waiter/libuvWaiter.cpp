73fd659655c89151bd2db3a4cc84e72e83f1ec08
Javier Serrano <js@mega.co.nz>
2017-07-20 12:08:06 +0200

    Cleanup

--- src/waiter/libuvWaiter.cpp
+++ src/waiter/libuvWaiter.cpp
@@ -1,44 +1,6 @@
-/**
- * @file posix/wait.cpp
- * @brief POSIX event/timeout handling
- *
- * (c) 2013-2014 by Mega Limited, Auckland, New Zealand
- *
- * This file is part of the MEGA SDK - Client Access Engine.
- *
- * Applications using the MEGA API must present a valid application key
- * and comply with the the rules set forth in the Terms of Service.
- *
- * The MEGA SDK is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *
- * @copyright Simplified (2-clause) BSD License.
- *
- * You should have received a copy of the license along with this
- * program.
- */
-
 #include "waiter/libuvWaiter.h"
 
-#ifdef __APPLE__
-#define CLOCK_MONOTONIC 0
-int clock_gettime(int, struct timespec* t)
-{
-    struct timeval now;
-    int rv = gettimeofday(&now, NULL);
-    if (rv)
-    {
-        return rv;
-    }
-    t->tv_sec = now.tv_sec;
-    t->tv_nsec = now.tv_usec * 1000;
-    return 0;
-}
-#endif
-
 namespace mega {
-dstime Waiter::ds;
 
 static void break_libuv_loop(uv_async_t* handle)
 {
@@ -66,14 +28,6 @@ LibuvWaiter::~LibuvWaiter()
 void LibuvWaiter::init(dstime ds)
 {
     Waiter::init(ds);
-}
-
-// update monotonously increasing timestamp in deciseconds
-void Waiter::bumpds()
-{
-    timespec ts;
-    clock_gettime(CLOCK_MONOTONIC, &ts);
-    ds = ts.tv_sec * 10 + ts.tv_nsec / 100000000;
 }
 
 int LibuvWaiter::wait()
--- src/waiter/libwebsocketsWaiter.cpp
+++ src/waiter/libwebsocketsWaiter.cpp
@@ -1,45 +1,7 @@
-/**
- * @file posix/wait.cpp
- * @brief POSIX event/timeout handling
- *
- * (c) 2013-2014 by Mega Limited, Auckland, New Zealand
- *
- * This file is part of the MEGA SDK - Client Access Engine.
- *
- * Applications using the MEGA API must present a valid application key
- * and comply with the the rules set forth in the Terms of Service.
- *
- * The MEGA SDK is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *
- * @copyright Simplified (2-clause) BSD License.
- *
- * You should have received a copy of the license along with this
- * program.
- */
-
 #include "waiter/libwebsocketsWaiter.h"
 #include <libwebsockets.h>
 
-#ifdef __APPLE__
-#define CLOCK_MONOTONIC 0
-int clock_gettime(int, struct timespec* t)
-{
-    struct timeval now;
-    int rv = gettimeofday(&now, NULL);
-    if (rv)
-    {
-        return rv;
-    }
-    t->tv_sec = now.tv_sec;
-    t->tv_nsec = now.tv_usec * 1000;
-    return 0;
-}
-#endif
-
 namespace mega {
-dstime Waiter::ds;
 
 LibwebsocketsWaiter::LibwebsocketsWaiter()
 {
@@ -54,16 +16,6 @@ LibwebsocketsWaiter::~LibwebsocketsWaiter()
 void LibwebsocketsWaiter::init(dstime ds)
 {
     Waiter::init(ds);
-}
-
-// update monotonously increasing timestamp in deciseconds
-void Waiter::bumpds()
-{
-    timespec ts;
-
-    clock_gettime(CLOCK_MONOTONIC, &ts);
-
-    ds = ts.tv_sec * 10 + ts.tv_nsec / 100000000;
 }
 
 int LibwebsocketsWaiter::wait()
