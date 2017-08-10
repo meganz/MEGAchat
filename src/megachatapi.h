dbe3a22e7eb97d94ce157de2c96672c7542089df
Sergio Hernández <shs@mega.nz>
2017-08-10 16:07:10 +0200

    Update doc

--- src/megachatapi.h
+++ src/megachatapi.h
@@ -1117,8 +1117,8 @@ public:
 
     enum
     {
-        INIT_ERROR                  = -1,   /// Initialization failed --> force a logout
-        INIT_WAITING_NEW_SESSION    = 1,    /// No \c sid provided at init() --> force a login
+        INIT_ERROR                  = -1,   /// Initialization failed --> disable chat
+        INIT_WAITING_NEW_SESSION    = 1,    /// No \c sid provided at init() --> force a login+fetchnodes
         INIT_OFFLINE_SESSION        = 2,    /// Initialization successful for offline operation
         INIT_ONLINE_SESSION         = 3,    /// Initialization successful for online operation --> login+fetchnodes completed
         INIT_NO_CACHE               = 7     /// Cache not available for \c sid provided --> remove SDK cache and force a login+fetchnodes
@@ -1211,9 +1211,15 @@ public:
     /**
      * @brief Initializes karere
      *
-     * If a session id is provided, karere will try to resume the session from cache.
-     * If no session is provided, karere will listen to login event in order to register a new
-     * session.
+     * If no session is provided, karere will listen to the fetchnodes event in order to register
+     * a new session and create its cache. It will return MegaChatApi::INIT_WAITING_NEW_SESSION.
+     *
+     * If a session id is provided, karere will try to resume the session from its cache and will
+     * return MegaChatApi::INIT_OFFLINE_SESSION.
+     *
+     * If a session id is provided but the correspoding cache is not available, it will return
+     * MegaChatApi::INIT_NO_CACHE and the app should go through a new fetchnodes in order to
+     * re-create a new cache from scratch.
      *
      * The initialization status is notified via `MegaChatListener::onChatInitStateUpdate`. See
      * the documentation of the callback for possible values.
@@ -1221,6 +1227,7 @@ public:
      * This function should be called before MegaApi::login and MegaApi::fetchnodes.
      *
      * @param sid Session id that wants to be resumed, or NULL if a new session will be created.
+     * @return The initialization state
      */
     int init(const char *sid);
 
