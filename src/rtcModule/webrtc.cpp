68c33e7e6f514b9c7949108aa3e2e529ed2e992d
Alberto Rodríguez <ar@mega.nz>
2018-02-28 16:53:02 +0100

    At onUserOffline, finish the call if state is equal to in-progress

--- src/rtcModule/webrtc.cpp
+++ src/rtcModule/webrtc.cpp
@@ -1456,7 +1456,7 @@ void Call::onUserOffline(Id userid, uint32_t clientid)
             }
         }
     }
-    else
+    else if (mState == kStateInProgress)
     {
         destroy(TermCode::kErrUserOffline, userid == mChat.client().karereClient->myHandle());
     }
