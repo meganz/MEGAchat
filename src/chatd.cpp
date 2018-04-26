ec0f4a94c97656bef5d70a55550e43e35243b661
Alberto Rodríguez <ar@mega.nz>
2018-04-26 17:51:36 +0200

    Allow upper case at urls

--- src/chatd.cpp
+++ src/chatd.cpp
@@ -3597,7 +3597,8 @@ bool Message::parseUrl(const std::string &url)
         return false;
     }
 
-    std::regex regularExpresion("^(http://www.|https://www.|http://|https://)?[a-z0-9]+([-.]{1}[a-z0-9]+)*.[a-z]{2,5}(:[0-9]{1,5})?(.*)?$");
+    std::regex regularExpresion("^(http://www.|https://www.|http://|https://)?[a-z0-9A-Z]+([-.]{1}[a-z0-9A-Z]+)*.[a-zA-Z]{2,5}(:[0-9]{1,5})?(.*)?$");
+
     return regex_match(url, regularExpresion);
 }
 }
