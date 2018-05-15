5ebf2b31ab01be51ef8624a60e34bd64c0a7635b
Sergio Hernández <shs@mega.nz>
2018-05-15 18:21:24 +0200

    Enable `ECHO` when offline
    In order to force a reconnect if not connected when app is moved to
    foreground (in background the OS may have inhibited the network
    connectivity and/or the execution power, which may result on server
    closing the socket), start a timer and reconnect unconditionally if
    timer expires.

--- src/chatd.cpp
+++ src/chatd.cpp
@@ -330,34 +330,32 @@ bool Connection::sendKeepalive(uint8_t opcode)
     return sendBuf(Command(opcode));
 }
 
-bool Connection::sendEcho()
+void Connection::sendEcho()
 {
     if (mEchoTimer) // one is already sent
-        return true;
+        return;
 
     CHATD_LOG_DEBUG("shard %d: send ECHO", mShardNo);
-    if (sendBuf(Command(OP_ECHO)))
+    sendBuf(Command(OP_ECHO));
+
+    auto wptr = weakHandle();
+    mEchoTimer = setTimeout([this, wptr]()
     {
-        auto wptr = weakHandle();
-        mEchoTimer = setTimeout([this, wptr]()
-        {
-            if (wptr.deleted())
-                return;
+        if (wptr.deleted())
+            return;
 
-            mEchoTimer = 0;
+        mEchoTimer = 0;
 
-            CHATD_LOG_DEBUG("Echo response not received in %d secs for shard %d. Reconnecting...", kEchoTimeout, mShardNo);
+        CHATD_LOG_DEBUG("Echo response not received in %d secs for shard %d. Reconnecting...", kEchoTimeout, mShardNo);
 
-            mState = kStateDisconnected;
-            mHeartbeatEnabled = false;
-            reconnect();
+        mState = kStateDisconnected;
+        mHeartbeatEnabled = false;
+        reconnect();
 
-        }, kEchoTimeout * 1000, mChatdClient.karereClient->appCtx);
+    }, kEchoTimeout * 1000, mChatdClient.karereClient->appCtx);
 
-        return true;
-    }
+    return;
 
-    return false;
 }
 
 Promise<void> Connection::reconnect()
