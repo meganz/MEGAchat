de5798ad13b03128f1dfbd096d7617da74894063
Javier Trujillo <jth@mega.nz>
2018-10-26 10:45:22 +0200

    Typo fixed in Objective-C bindings

--- bindings/Objective-C/MEGAChatRequest.h
+++ bindings/Objective-C/MEGAChatRequest.h
@@ -31,7 +31,6 @@ typedef NS_ENUM (NSInteger, MEGAChatRequestType) {
     MEGAChatRequestTypeSetPresencePersist,
     MEGAChatRequestTypeSetPresenceAutoaway,
     MEGAChatRequestTypeLoadAudioVideoDevices,
-    MEGAChatMessageTypeArchiveChatRoom,
     MEGAChatRequestTypeArchiveChatRoom,
     MEGAChatRequestTypePushReceived,
     MEGAChatRequestTypeLoadChatLink,
