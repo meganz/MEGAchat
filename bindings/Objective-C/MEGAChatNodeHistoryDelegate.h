9b6b330a6cf9c128d4cc62d8be136bd17e3af5ed
Javier Navarro <jn@mega.co.nz>
2018-11-09 14:11:52 +0100

    Chat node history (Obj-C)

---
+++ bindings/Objective-C/MEGAChatNodeHistoryDelegate.h
@@ -0,0 +1,17 @@
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@protocol MEGAChatNodeHistoryDelegate <NSObject>
+
+@optional
+
+- (void)onAttachmentLoaded:(MEGAChatSdk *)api message:(MEGAChatMessage *)message;
+- (void)onAttachmentReceived:(MEGAChatSdk *)api message:(MEGAChatMessage *)message;
+- (void)onAttachmentDeleted:(MEGAChatSdk *)api messageId:(uint64_t)messageId;
+- (void)onTruncate:(MEGAChatSdk *)api messageId:(uint64_t)messageId;
+
+@end
+
+NS_ASSUME_NONNULL_END
