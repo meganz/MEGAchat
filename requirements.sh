627dedc59b5fbdfc156aece1870e98ed9e79c062
rpotter12 <rohitpotter12@gmail.com>
2019-01-01 20:34:08 +0530

    adding requirements installation file

---
+++ requirements.sh
@@ -0,0 +1,22 @@
+sudo apt-get update
+
+# cmake
+sudo pip install --upgrade cmake
+
+# ccmake
+sudo apt-get install cmake-curses-gui
+
+# autoconf
+sudo apt-get install autoconf
+
+# automake
+sudo apt-get install automake
+
+# libtool
+sudo apt-get install libtool
+
+# python
+sudo apt-get install python2.7
+
+# g++
+sudo apt-get install g++
