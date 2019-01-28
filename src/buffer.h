537a0fc280aff1528721fc909b7cf57cbb9a1f16
Pablo Martin <pm@mega.nz>
2019-01-28 12:57:14 +0100

    remove unexpected non-printable character

--- src/buffer.h
+++ src/buffer.h
@@ -6,7 +6,7 @@
 #include <string.h>
 #include <vector>
 
-#if !defined(__arm__) && !defined(__aarch64__)
+#if !defined(__arm__) && !defined(__aarch64__)
     #define BUFFER_ALLOW_UNALIGNED_MEMORY_ACCESS 1
 #endif
 class BufferRangeError: public std::runtime_error
