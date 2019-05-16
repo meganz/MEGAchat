5374cc7622570d1f1f8c5e71638db6d5f34040a9
Javier Navarro <jn@mega.co.nz>
2019-05-16 14:05:37 +0200

    Update MEGAChatCallStatus enum with reconnecting (Obj-C)

--- bindings/Objective-C/MEGAChatCall.h
+++ bindings/Objective-C/MEGAChatCall.h
@@ -12,7 +12,8 @@ typedef NS_ENUM (NSInteger, MEGAChatCallStatus) {
     MEGAChatCallStatusInProgress,
     MEGAChatCallStatusTerminatingUserParticipation,
     MEGAChatCallStatusDestroyed,
-    MEGAChatCallStatusUserNoPresent
+    MEGAChatCallStatusUserNoPresent,
+    MEGAChatCallStatusReconnecting
 };
 
 typedef NS_ENUM (NSInteger, MEGAChatCallSessionStatus) {
