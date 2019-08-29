96ef55620936c37bdf6a45a09351558fbf7d1ec4
Javier Gonzalez Andres <jga@mega.nz>
2019-08-29 11:22:31 +0200

    Add a list of predefined reactions in QtApp settings

--- examples/qtmegachatapi/uiSettings.cpp
+++ examples/qtmegachatapi/uiSettings.cpp
@@ -21,3 +21,16 @@ QColor gAvatarColors[16] = {
     "cadetblue", "#db00d3", "darkturquoise", "lightblue",
     "honeydew", "lightyellow", "violet", "turquoise"
 };
+
+QStringList utf8reactionsList = {
+    QString::fromUtf8(QByteArray::fromHex("F0 9F 98 80")),
+    QString::fromUtf8(QByteArray::fromHex("F0 9F 98 81")),
+    QString::fromUtf8(QByteArray::fromHex("F0 9F 98 82")),
+    QString::fromUtf8(QByteArray::fromHex("F0 9F 98 83")),
+    QString::fromUtf8(QByteArray::fromHex("F0 9F 98 84")),
+    QString::fromUtf8(QByteArray::fromHex("F0 9F 98 85")),
+    QString::fromUtf8(QByteArray::fromHex("F0 9F 98 86")),
+    QString::fromUtf8(QByteArray::fromHex("F0 9F 98 87")),
+    QString::fromUtf8(QByteArray::fromHex("F0 9F 98 88")),
+    QString::fromUtf8(QByteArray::fromHex("F0 9F 98 89"))
+};
