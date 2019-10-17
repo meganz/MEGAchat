5fe874a55616d3eae261bcf56c4b506d1b740d92
Sergio Hernández <shs@mega.nz>
2019-10-17 14:33:42 +0200

    Protect against calls without audio track

--- src/rtcModule/webrtc.cpp
+++ src/rtcModule/webrtc.cpp
@@ -2861,7 +2861,11 @@ void Session::onAddStream(artc::tspMediaStream stream)
     });
     mRemotePlayer->attachToStream(stream);
     mRemotePlayer->enableVideo(mPeerAv.video());
-    mRemotePlayer->getAudioTrack()->AddSink(mAudioLevelMonitor.get());
+
+    if (mRemotePlayer->isAudioAttached())
+    {
+        mRemotePlayer->getAudioTrack()->AddSink(mAudioLevelMonitor.get());
+    }
 }
 void Session::onRemoveStream(artc::tspMediaStream stream)
 {
@@ -2872,7 +2876,10 @@ void Session::onRemoveStream(artc::tspMediaStream stream)
     }
     if(mRemotePlayer)
     {
-        mRemotePlayer->getAudioTrack()->RemoveSink(mAudioLevelMonitor.get());
+        if (mRemotePlayer->isAudioAttached())
+        {
+            mRemotePlayer->getAudioTrack()->RemoveSink(mAudioLevelMonitor.get());
+        }
         mRemotePlayer->detachFromStream();
         mRemotePlayer.reset();
     }
