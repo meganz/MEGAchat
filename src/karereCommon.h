d5c7f0563d8e29fa0dceea26e1d7e9cdef1baf05
Sergio Hernández <shs@mega.nz>
2020-04-15 20:10:14 +0200

    Update static TURN server
    001 went offline

--- src/karereCommon.h
+++ src/karereCommon.h
@@ -51,7 +51,7 @@
 #define KARERE_DEFAULT_TURN_SERVERS \
    "[{\"host\":\"turn:trn270n001.karere.mega.nz:3478?transport=udp\"}," \
     "{\"host\":\"turn:trn302n001.karere.mega.nz:3478?transport=udp\"}," \
-    "{\"host\":\"turn:trn530n001.karere.mega.nz:3478?transport=udp\"}]"
+    "{\"host\":\"turn:trn530n002.karere.mega.nz:3478?transport=udp\"}]"
 
 #define KARERE_TURN_USERNAME "inoo20jdnH"
 #define KARERE_TURN_PASSWORD "02nNKDBkkS"
