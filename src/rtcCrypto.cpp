d013e192425eca3e49a0bc4155b9760fadcd9304
Javier Gonzalez Andres <jga@mega.nz>
2021-01-05 15:30:43 +0100

    Add class RtcCryptoMeetings to implement IRtcCryptoMeetings

--- src/rtcCrypto.cpp
+++ src/rtcCrypto.cpp
@@ -75,4 +75,59 @@ void RtcCrypto::random(char* buf, size_t size)
     ::mega::PrnGen rng;
     rng.genblock((unsigned char*)buf, size);
 }
+
+RtcCryptoMeetings::RtcCryptoMeetings(karere::Client& client)
+:mClient(client)
+{
+
+}
+
+void RtcCryptoMeetings::computeSymmetricKey(Id peer, strongvelope::SendKey &output)
+{
+    auto pms = mClient.userAttrCache().getAttr(peer, ::mega::MegaApi::USER_ATTR_CU25519_PUBLIC_KEY);
+    if (!pms.done())
+        throw std::runtime_error("RtcCrypto::computeSymmetricKey: Key not readily available in cache");
+    if (pms.failed())
+        throw std::runtime_error("RtcCrypto:computeSymmetricKey: Error getting key for user "+ peer.toString()+" :"+pms.error().msg());
+    Buffer* pubKey = pms.value();
+    if (pubKey->empty())
+        throw std::runtime_error("RtcCrypto:computeSymmetricKey: Empty Cu25519 chat key for user "+peer.toString());
+    strongvelope::Key<crypto_scalarmult_BYTES> sharedSecret;
+    auto ignore = crypto_scalarmult(sharedSecret.ubuf(), (const unsigned char*)mClient.mMyPrivCu25519, pubKey->ubuf());
+    (void)ignore;
+    strongvelope::deriveSharedKey(sharedSecret, output, "webrtc pairwise key\x01");
+}
+
+void RtcCryptoMeetings::decryptKeyFrom(const karere::Id &peer, const strongvelope::SendKey &data, strongvelope::SendKey &output)
+{
+    strongvelope::SendKey aesKey;
+    computeSymmetricKey(peer, aesKey);
+    assert(aesKey.dataSize() == 16);
+    ECB_Mode<AES>::Decryption aesdecryption(aesKey.ubuf(), aesKey.dataSize());
+    aesdecryption.ProcessData(output.ubuf(), data.ubuf(), data.dataSize());
+}
+
+void RtcCryptoMeetings::encryptKeyTo(const karere::Id &peer, const strongvelope::SendKey &data, strongvelope::SendKey &output)
+{
+    strongvelope::SendKey aesKey;
+    computeSymmetricKey(peer, aesKey);
+    assert(aesKey.dataSize() == 16);
+    ECB_Mode<AES>::Encryption aesencryption(aesKey.ubuf(), aesKey.dataSize());
+    aesencryption.ProcessData(output.ubuf(), data.ubuf(), data.dataSize());
+
+}
+
+std::shared_ptr<strongvelope::SendKey> RtcCryptoMeetings::generateSendKey()
+{
+    std::shared_ptr<strongvelope::SendKey> key;
+    key.reset(new strongvelope::SendKey);
+    key->setDataSize(AES::BLOCKSIZE);
+    randombytes_buf(key->ubuf(), AES::BLOCKSIZE);
+    return key;
+}
+
+void RtcCryptoMeetings::xorWithCallKey(const strongvelope::SendKey &callKey, strongvelope::SendKey &sendKey)
+{
+    SymmCipher::xorblock(static_cast<const byte *> (callKey.ubuf()), static_cast<byte *>(sendKey.ubuf()));
+}
 }
