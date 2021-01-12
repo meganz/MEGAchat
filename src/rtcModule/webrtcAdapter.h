9c32c2333397bc538a9da882fee774b1ca92549a
Alberto Rodríguez <ar@mega.nz>
2021-01-12 17:44:27 +0100

    Fix compilation issues at IOS

--- src/rtcModule/webrtcAdapter.h
+++ src/rtcModule/webrtcAdapter.h
@@ -327,7 +327,7 @@ public:
     bool is_screencast() const override;
     absl::optional<bool> needs_denoising() const override;
 
-    bool SupportsEncodedOutput() const override {}
+    bool SupportsEncodedOutput() const override { return  false; }
     void GenerateKeyFrame() override {}
 
     bool GetStats(webrtc::VideoTrackSourceInterface::Stats* stats) override;
@@ -381,6 +381,12 @@ public:
 
     void RegisterObserver(webrtc::ObserverInterface* observer) override;
     void UnregisterObserver(webrtc::ObserverInterface* observer) override;
+
+    bool SupportsEncodedOutput() const override;
+    void GenerateKeyFrame() override;
+
+    void AddEncodedSink(rtc::VideoSinkInterface<webrtc::RecordableEncodedFrame>* sink) override;
+    void RemoveEncodedSink(rtc::VideoSinkInterface<webrtc::RecordableEncodedFrame>* sink) override;
     
 private:
     bool mRunning = false;
