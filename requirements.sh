965634f28c0aec5de35796e81b0f146b8c804eb5
Catalin <cra@mega.nz>
2021-02-10 11:52:52 +0200

    Improve MEGAchat build instructions

--- requirements.sh
+++ requirements.sh
@@ -20,3 +20,15 @@ sudo apt-get install python2.7
 
 # g++
 sudo apt-get install g++
+
+# gtest
+sudo apt-get install libgtest-dev
+cd /usr/src/gtest
+sudo cmake CMakeLists.txt
+sudo make
+sudo cp lib/*.a /usr/lib
+#sudo ln -s /usr/src/gtest/lib/libgtest.a /usr/lib/libgtest.a
+#sudo ln -s /usr/src/gtest/lib/libgtest_main.a /usr/lib/libgtest_main.a
+
+# other libs (Ubuntu 20.04.2)
+sudo apt-get install libglib2.0-dev libgtk-3-dev libpulse-dev libasound2-dev libuv1-dev libcap-dev libfreeimage-dev ffmpeg
