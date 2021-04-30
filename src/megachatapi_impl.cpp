ff9203750af76d6a835805684806f938fa97f369
Alberto Rodríguez <ar@mega.nz>
2021-04-30 08:37:22 +0200

    Change position of  this->setParamType(request.getParamType());

--- src/megachatapi_impl.cpp
+++ src/megachatapi_impl.cpp
@@ -5275,7 +5275,6 @@ MegaChatRequestPrivate::MegaChatRequestPrivate(MegaChatRequestPrivate &request)
     this->setMegaChatMessage(request.getMegaChatMessage());
     this->setMegaNodeList(request.getMegaNodeList());
     this->setMegaHandleList(request.getMegaHandleList());
-    this->setParamType(request.getParamType());
     if (mMegaHandleList)
     {
         for (unsigned int i = 0; i < mMegaHandleList->size(); i++)
@@ -5284,6 +5283,8 @@ MegaChatRequestPrivate::MegaChatRequestPrivate(MegaChatRequestPrivate &request)
             this->setMegaHandleListByChat(chatid, request.getMegaHandleListByChat(chatid));
         }
     }
+
+    this->setParamType(request.getParamType());
 }
 
 MegaChatRequestPrivate::~MegaChatRequestPrivate()
