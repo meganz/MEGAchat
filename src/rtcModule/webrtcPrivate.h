e854b0df32d6740c4afbeea2e5d4e2e00034a3f3
Alberto Rodríguez <ar@mega.nz>
2021-09-24 12:56:39 +0200

    Remove unused code (AvailableTracks)

--- src/rtcModule/webrtcPrivate.h
+++ src/rtcModule/webrtcPrivate.h
@@ -17,37 +17,6 @@ class RtcModuleSfu;
 class Call;
 class Session;
 
-/*
- * This class represents the current available tracks keyed by peer CID.
- * Available tracks information is stored into a karere::AvFlags struct (due to efficiency)
- *
- * When a track is enabled/disabled we will update CID flags, and when we want to query which tracks
- * are available, we will check if CID flags contains these values:
- *  - HI-RES  track -> kCameraHiRes
- *  - LOW-RES track -> kCameraLowRes
- *  - AUDIO   track -> kAudio
- */
-class AvailableTracks
-{
-public:
-    AvailableTracks();
-    ~AvailableTracks();
-    bool hasHiresTrack(Cid_t cid);
-    bool hasLowresTrack(Cid_t cid);
-    bool hasVoiceTrack(Cid_t cid);
-    void updateHiresTrack(Cid_t cid, bool add);
-    void updateLowresTrack(Cid_t cid, bool add);
-    void updateSpeakTrack(Cid_t cid, bool add);
-    std::map<Cid_t, karere::AvFlags>& getTracks();
-    bool getTracksByCid(Cid_t cid, karere::AvFlags& tracksFlags);
-    void addCid(Cid_t cid);
-    void removeCid(Cid_t cid);
-    bool hasCid(Cid_t cid);
-    void clear();
-private:
-    std::map<Cid_t, karere::AvFlags> mTracksFlags;
-};
-
 class AudioLevelMonitor : public webrtc::AudioTrackSinkInterface, public karere::DeleteTrackable
 {
     public:
