462f3d33f44c87315ee5a10a1386ae812ee038a9
Javier Gonzalez Andres <jga@mega.nz>
2022-05-03 11:16:08 +0200

    Add rsn param to PEERLEFT command at UNITARYTEST_SfuDataReception

--- tests/sdk_test/sdk_test.cpp
+++ tests/sdk_test/sdk_test.cpp
@@ -5425,7 +5425,7 @@ bool MegaChatApiUnitaryTest::UNITARYTEST_SfuDataReception()
     checkCommands["{\"cmd\":\"AV\",\"cid\":\"sdfasdfas\",\"peer\":\"dsfasdfas\",\"av\":1}"]     = false;
     checkCommands["{\"cmd\":\"AV\",\"cid\":\"sdfasdfas\",\"peer\":"]                            = false;
     checkCommands["{\"a\":\"HIRES_STOP\"}"]                                                     = true;
-    checkCommands["{\"a\":\"PEERLEFT\",\"cid\":2}"]                                             = true;
+    checkCommands["{\"a\":\"PEERLEFT\",\"cid\":2,\"rsn\":65}"]                                  = true;
     checkCommands["{\"a\":\"PEERJOIN\",\"cid\":2,\"userId\":\"amECEsVQJQ8\",\"av\":0}"]         = true;
     checkCommands["{\"a\":\"HIRES_START\"}"]                                                    = true;
     checkCommands["{\"a\":\"ERR\",\"code\":129,\"msg\":\"Error\"}"]                             = false;
