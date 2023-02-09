87341f808a6876773b1f7ef17892b926635e2d84
Carlos Martín Acera <cma@mega.nz>
2023-02-09 10:34:57 +0100

    Remove createChatAndScheduledMeeting iOS bindings

--- bindings/Objective-C/MEGAChatSdk.mm
+++ bindings/Objective-C/MEGAChatSdk.mm
@@ -1380,16 +1380,6 @@ static DelegateMEGAChatLoggerListener *externalLogger = NULL;
 
 #pragma mark - Scheduled meetings
 
-- (void)createChatAndScheduledMeeting:(BOOL)isMeeting publicChat:(BOOL)publicChat speakRequest:(BOOL)speakRequest waitingRoom:(BOOL)waitingRoom openInvite:(BOOL)openInvite timezone:(NSString *)timezone startDate:(uint64_t)startDate endDate:(uint64_t)endDate title:(NSString *)title description:(NSString *)description emailsDisabled:(BOOL)emailsDisabled frequency:(int)frequency attributes:(NSString *)attributes {
-    if (!self.megaChatApi) { return; }
-    self.megaChatApi -> createChatAndScheduledMeeting(isMeeting, publicChat, speakRequest, waitingRoom, openInvite, timezone.UTF8String, startDate, endDate, title.UTF8String, description.UTF8String, MegaChatScheduledFlags::createInstance(), MegaChatScheduledRules::createInstance(frequency), attributes.UTF8String);
-}
-
-- (void)createChatAndScheduledMeeting:(BOOL)isMeeting publicChat:(BOOL)publicChat speakRequest:(BOOL)speakRequest waitingRoom:(BOOL)waitingRoom openInvite:(BOOL)openInvite timezone:(NSString *)timezone startDate:(uint64_t)startDate endDate:(uint64_t)endDate title:(NSString *)title description:(NSString *)description emailsDisabled:(BOOL)emailsDisabled frequency:(int)frequency attributes:(NSString *)attributes delegate:(id<MEGAChatRequestDelegate>)delegate {
-    if (!self.megaChatApi) { return; }
-    self.megaChatApi -> createChatAndScheduledMeeting(isMeeting, publicChat, speakRequest, waitingRoom, openInvite, timezone.UTF8String, startDate, endDate, title.UTF8String, description.UTF8String, MegaChatScheduledFlags::createInstance(), MegaChatScheduledRules::createInstance(frequency), attributes.UTF8String, [self createDelegateMEGAChatRequestListener:delegate singleListener:YES]);
-}
-
 - (void)updateScheduledMeeting:(uint64_t)chatId scheduledId:(uint64_t)scheduledId timezone:(NSString *)timezone startDate:(uint64_t)startDate endDate:(uint64_t)endDate title:(NSString *)title description:(NSString *)description cancelled:(BOOL)cancelled emailsDisabled:(BOOL)emailsDisabled frequency:(int)frequency attributes:(NSString *)attributes {
     if (!self.megaChatApi) { return; }
     self.megaChatApi->updateScheduledMeeting(chatId, scheduledId, timezone.UTF8String, startDate, endDate, title.UTF8String, description.UTF8String, cancelled, MegaChatScheduledFlags::createInstance(), MegaChatScheduledRules::createInstance(frequency));
