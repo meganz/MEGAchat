3636eacc7ceee04f20de1abd1ccade820417eea5
Javier Gonzalez Andres <jga@mega.nz>
2023-11-16 16:14:40 +0100

    Tify code at getEphemeralPubKeyDerived
    First check for v1 and then isKnownSfuVersion getEphemeralPubKeyDerived

--- src/sfu.cpp
+++ src/sfu.cpp
@@ -182,22 +182,22 @@ std::string Peer::getEphemeralPubKeyDerived() const
 
 bool Peer::setEphemeralPubKeyDerived(const std::string& key)
 {
+    if (getPeerSfuVersion() == sfu::SfuProtocol::SFU_PROTO_V1)
+    {
+        // we shouldn't receive any peer with protocol v1
+        SFU_LOG_WARNING("setEphemeralPubKeyDerived: unexpected SFU protocol version [%u] for user: %s, cid: %u",
+                        static_cast<std::underlying_type<sfu::SfuProtocol>::type>(getPeerSfuVersion()),
+                        getPeerid().toString().c_str(), getCid());
+        assert(false);
+        return false;
+    }
+
     if (!sfu::isKnownSfuVersion(getPeerSfuVersion()))
     {
         // important: upon an unkown peers's SFU protocol version, native client should act as if they are the latest known version
         SFU_LOG_WARNING("setEphemeralPubKeyDerived: unknown SFU protocol version [%u] for user: %s, cid: %u",
                          static_cast<std::underlying_type<sfu::SfuProtocol>::type>(getPeerSfuVersion()),
                          getPeerid().toString().c_str(), getCid());
-    }
-
-    if (getPeerSfuVersion() == sfu::SfuProtocol::SFU_PROTO_V1)
-    {
-        // we shouldn't receive any peer with protocol v1
-        SFU_LOG_WARNING("setEphemeralPubKeyDerived: unexpected SFU protocol version [%u] for user: %s, cid: %u",
-                       static_cast<std::underlying_type<sfu::SfuProtocol>::type>(getPeerSfuVersion()),
-                       getPeerid().toString().c_str(), getCid());
-        assert(false);
-        return false;
     }
 
     if (key.empty() && getPeerSfuVersion() > sfu::SfuProtocol::SFU_PROTO_V0)
