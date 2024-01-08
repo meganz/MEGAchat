316fb99d67fd6a19bfbcd1534dc8f1a767da5796
Javier Gonzalez Andres <jga@mega.nz>
2024-01-08 09:50:26 +0100

    Fix open chat link issues
    Upon opening a chat-link already opened previously, onChatRequestFinish
    returned ERROR_OK, but apps expected ERROR_EXIST

--- src/megachatapi_impl.cpp
+++ src/megachatapi_impl.cpp
@@ -1117,54 +1117,53 @@ int MegaChatApiImpl::performRequest_loadPreview(MegaChatRequestPrivate *request)
                    }
                    else //Load chat link
                    {
-                       Id ph = result->getNodeHandle();
-                       request->setUserHandle(ph.val);
-                       GroupChatRoom* room = dynamic_cast<GroupChatRoom *> (findChatRoom(chatId));
-                       const bool hasChanged = room && room->hasChatLinkChanged(ph.val, decryptedTitle, meeting, opts);
-                       std::string url = result->getLink() ? result->getLink() : "";
-                       int shard = result->getAccess();
-
-                       if (room && !hasChanged)
-                       {
-                           int err = MegaChatError::ERROR_EXIST;
-                           const bool enablePreview = !room->isActive() && room->previewMode();
-                           if (enablePreview)
-                           {
-                               err = MegaChatError::ERROR_OK;
-                               room->enablePreview(ph);
-                           }
-
-                           // update sched meetings if necessary and notify app
-                           room->updateSchedMeetingsWithList(smList);
-                           MegaChatErrorPrivate* megaChatError = new MegaChatErrorPrivate(err);
+                        GroupChatRoom* room = dynamic_cast<GroupChatRoom *> (findChatRoom(chatId));
+                        if (!room && mClient->mChatdClient->chatFromId(chatId))
+                        {
+                           assert(!mClient->mChatdClient->chatFromId(chatId));
+                           API_LOG_ERROR("Chatid (%s) already exists at mChatForChatId but not at ChatRoomList", karere::Id(chatId).toString().c_str());
+                           MegaChatErrorPrivate* megaChatError = new MegaChatErrorPrivate(MegaChatError::ERROR_NOENT);
                            fireOnChatRequestFinish(request, megaChatError);
-                       }
-                       else
-                       {
-                           if (!room && mClient->mChatdClient->chatFromId(chatId))
-                           {
-                              assert(!mClient->mChatdClient->chatFromId(chatId));
-                              API_LOG_ERROR("Chatid (%s) already exists at mChatForChatId but not at ChatRoomList", karere::Id(chatId).toString().c_str());
-                              MegaChatErrorPrivate *megaChatError = new MegaChatErrorPrivate(MegaChatError::ERROR_EXIST);
-                              fireOnChatRequestFinish(request, megaChatError);
-                              return;
-                           }
+                           return;
+                        }
 
-                           if (hasChanged)
-                           {
-                              assert(room);
-                              // if mcphurl information is different respect GroupChatRoom in ram
-                              // we need to remove preview and recreate again, this is simplier than update
-                              // groupchatroom field by field
-                              mClient->chats->removeRoomPreview(chatId);
-                           }
+                        const Id& ph = result->getNodeHandle();
+                        request->setUserHandle(ph.val);
+                        const bool hasChanged = room && room->hasChatLinkChanged(ph.val, decryptedTitle, meeting, opts);
+                        const std::string url = result->getLink() ? result->getLink() : "";
+                        const int shard = result->getAccess();
+                        const uint32_t ts = static_cast<uint32_t>(result->getNumber());
+                        std::shared_ptr<std::string> key = std::make_shared<std::string>(unifiedKey);
 
-                           std::shared_ptr<std::string> key = std::make_shared<std::string>(unifiedKey);
-                           uint32_t ts = static_cast<uint32_t>(result->getNumber());
-                           mClient->createPublicChatRoom(chatId, ph.val, shard, decryptedTitle, key, url, ts, meeting, opts, smList);
-                           MegaChatErrorPrivate *megaChatError = new MegaChatErrorPrivate(MegaChatError::ERROR_OK);
-                           fireOnChatRequestFinish(request, megaChatError);
-                       }
+                        if (room) // chatroom already exists
+                        {
+                            if (hasChanged)
+                            {
+                                // if mcphurl information is different respect GroupChatRoom in ram
+                                // we need to remove preview and recreate again, this is simplier than update
+                                // groupchatroom field by field
+                                API_LOG_DEBUG("Chat (%s) has changed", karere::Id(chatId).toString().c_str());
+                                mClient->chats->removeRoomPreview(chatId);
+                                mClient->createPublicChatRoom(chatId, ph.val, shard, decryptedTitle, key, url, ts, meeting, opts, smList);
+                            }
+                            else if (!room->isActive() && room->previewMode()) // re-enable preview
+                            {
+                                API_LOG_DEBUG("Re-enable chat (%s) preview", karere::Id(chatId).toString().c_str());
+                                room->enablePreview(ph);
+                            }
+
+                            // update sched meetings if necessary and notify app
+                            API_LOG_ERROR("Chatid (%s) already exists", karere::Id(chatId).toString().c_str());
+                            room->updateSchedMeetingsWithList(smList);
+                            MegaChatErrorPrivate* megaChatError = new MegaChatErrorPrivate(MegaChatError::ERROR_EXIST);
+                            fireOnChatRequestFinish(request, megaChatError);
+                            return;
+                        }
+
+                        // create public chat room from info received from API
+                        mClient->createPublicChatRoom(chatId, ph.val, shard, decryptedTitle, key, url, ts, meeting, opts, smList);
+                        MegaChatErrorPrivate* megaChatError = new MegaChatErrorPrivate(MegaChatError::ERROR_OK);
+                        fireOnChatRequestFinish(request, megaChatError);
                    }
                 })
                 .fail([request, this](const ::promise::Error& err)
