bb09f11d0a77eebca026d0d31bcc29e592edb631
Jose Manuel Otero Mato <jom@mega.co.nz>
2024-01-29 11:53:46 +0100

    Changed the DebugOutputOptions name and added some documentation

--- examples/megaclc/megaclc.cpp
+++ examples/megaclc/megaclc.cpp
@@ -338,9 +338,33 @@ std::string timeToLocalTimeString(const int64_t time)
     return std::string{buffer};
 }
 
-class DebugOutputOptions
+/**
+ * @brief This class is meant to manage the writing of the log/debug messages by the Loggers (MegaCLLogger and
+ * MegaclcChatChatLogger) into a file and/or the cout.
+ *
+ * As it is thought to be used by multiple loggers that may be running in different threads it uses a mutex to access
+ * the attributes.
+ *
+ * Usage: There is a variety of methods to enable/disable the writing to a file or to cout and also the minimum level of
+ * severity of the messages to write. However the main method to use this class is the `writeOutput`. You should call
+ * the method through an instance of this class in a global scope, accessible to all the Loggers you want to coordinate.
+ */
+class DebugOutputWriter
 {
 public:
+    /**
+     * @brief Writes the given message to the file if it was set and to the cout if it was enable.
+     *
+     * NOTE: The logLevel parameter is compared against an internal level that you can change with the `setLogLevel`
+     * method and which has the value of 1 by default (that matches with the `logError` value). If the input
+     * logLevel is larger than the internal one the message is not processed. Else, the message will always be written
+     * into the file if it was enable but it will be only written to the cout if the logLevel is associated to the error
+     * or fatal error level. This is done to avoid massive writing to the cout which could occasionally difficult the
+     * command input.
+     *
+     * @param msg The message to write
+     * @param logLevel The severity of the message. See the `LogLevel` enum in the `mega/logging.h` file.
+     */
     void writeOutput(const std::basic_string<char>& msg, int logLevel)
     {
         std::lock_guard<std::mutex>lock{logFileWriteMutex};
@@ -369,6 +393,15 @@ public:
         logFile.close();
     }
 
+    /**
+     * @brief Enables the writing to a file.
+     *
+     * If a file was already opened, it will be closed before opening the new one.
+     *
+     * NOTE: If the file name to open matches the previously opened file, it will be overwritten.
+     *
+     * @param fname The path to the file to write the messages.
+     */
     void enableLogToFile(const std::string& fname)
     {
         std::lock_guard<std::mutex>lock{logFileWriteMutex};
@@ -389,7 +422,7 @@ public:
         }
     }
 
-    bool isLoggingToFile() const 
+    bool isLoggingToFile() const
     {
         std::lock_guard<std::mutex>lock{logFileWriteMutex};
         return logFile.is_open();
@@ -428,7 +461,7 @@ private:
     bool logToConsole = false;
 };
 
-DebugOutputOptions g_reviewPublicChatOutOptions;
+DebugOutputWriter g_reviewPublicChatOutOptions;
 
 class MegaCLLogger : public m::Logger {
 public:
