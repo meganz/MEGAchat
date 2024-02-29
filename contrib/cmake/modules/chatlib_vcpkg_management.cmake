e7b9b50408dbfbfbcc8ec01bc6e8b6119f482353
Alfonso Abella <aag@mega.co.nz>
2024-02-29 18:05:05 +0100

    Reorganized WebRTC dependencies

--- contrib/cmake/modules/chatlib_vcpkg_management.cmake
+++ contrib/cmake/modules/chatlib_vcpkg_management.cmake
@@ -2,6 +2,8 @@ macro(process_chatlib_vcpkg_libraries)
 
     if (USE_WEBRTC)
         list(APPEND VCPKG_MANIFEST_FEATURES "use-webrtc")
+    else()
+        list(APPEND VCPKG_MANIFEST_FEATURES "no-webrtc")
     endif()
 
     if (ENABLE_CHATLIB_TESTS)
