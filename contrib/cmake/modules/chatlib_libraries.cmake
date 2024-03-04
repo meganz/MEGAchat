61576af2c35052281436894077c079efa348ec22
Alfonso Abella <aag@mega.co.nz>
2024-03-04 19:06:40 +0100

    Link with WebRTC
    - Added overlay directory to find WebRTC library
    - Added find_package to start using and linking with the WebRTC library
    - Adjust PRIVATE to PUBLIC include directories now needed.
    - Adjusted vcpkg.json to use BoringSSL when WebRTC is enabled

--- contrib/cmake/modules/chatlib_libraries.cmake
+++ contrib/cmake/modules/chatlib_libraries.cmake
@@ -3,4 +3,7 @@ macro(load_chatlib_libraries)
     find_package(libwebsockets CONFIG REQUIRED)
     target_link_libraries(CHATlib PRIVATE websockets)
 
+    find_package(webrtc CONFIG REQUIRED)
+    target_link_libraries(CHATlib PUBLIC WEBRTC::webrtc)
+
 endmacro()
