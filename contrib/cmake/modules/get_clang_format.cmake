5127570a48ecbd0c5aa93230fbebe052bbdbb1ee
Antonio Borondo <abo@mega.co.nz>
2024-08-02 17:13:23 +0200

    Download .clang-format with Git archive

--- contrib/cmake/modules/get_clang_format.cmake
+++ contrib/cmake/modules/get_clang_format.cmake
@@ -1,7 +1,24 @@
 function(get_clang_format)
     message(STATUS "Downloading .clang-format")
 
-    set(URL "https://code.developers.mega.co.nz/sdk/sdk/-/raw/develop/.clang-format")
-    set(DESTINATION_PATH ${PROJECT_SOURCE_DIR}/.clang-format)
-    file(DOWNLOAD ${URL} ${DESTINATION_PATH} SHOW_PROGRESS)
+    # Download SDK repo archive with only .clang-format
+    execute_process(
+        COMMAND git archive --output=clang-format.tar --remote=git@code.developers.mega.co.nz:sdk/sdk.git HEAD .clang-format
+        WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+        OUTPUT_QUIET
+    )
+
+    # Extract archive
+    execute_process(
+        COMMAND ${CMAKE_COMMAND} -E tar xvf clang-format.tar
+        WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+        OUTPUT_QUIET
+    )
+
+    # Remove archive
+    execute_process(
+        COMMAND ${CMAKE_COMMAND} -E remove clang-format.tar
+        WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+        OUTPUT_QUIET
+    )
 endfunction()
