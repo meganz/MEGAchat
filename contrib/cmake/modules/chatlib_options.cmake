cfd52dd28a07515c5ba3ce22662272e1b85fa1f8
Antonio Borondo <abo@mega.co.nz>
2025-06-03 12:06:55 +0200

    Adjust ENABLE_QT_BINDINGS option

--- contrib/cmake/modules/chatlib_options.cmake
+++ contrib/cmake/modules/chatlib_options.cmake
@@ -6,12 +6,6 @@ else() # Win and mac
     option(USE_WEBRTC "Support for voice and/or video calls" OFF)
 endif()
 
-if(ENABLE_CHATLIB_QTAPP)
-    option(ENABLE_QT_BINDINGS "Enable the target to build the Qt Bindings" ON)
-else()
-    option(ENABLE_QT_BINDINGS "Enable the target to build the Qt Bindings" OFF)
-endif()
-
 # MEGAsdk options
 # Configure MEGAsdk specific options for MEGAchat and then load the rest of MEGAsdk configuration
 set(ENABLE_CHAT ON) # Chat management functionality.
@@ -21,5 +15,6 @@ endif()
 
 option(ENABLE_CHATLIB_QTAPP "Qt example app is built if enabled" OFF)
 option(ENABLE_JAVA_BINDINGS "Enable the target to build the Java Bindings" OFF)
+option(ENABLE_QT_BINDINGS "Enable the target to build the Qt Bindings" OFF)
 
 include(sdklib_options)
